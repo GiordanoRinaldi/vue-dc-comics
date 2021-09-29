dfb6ba5c24e4b9936c2b81d9dd6be168e1382c0d
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-30 00:09:09 +0200

    sezione "top"

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,5 +1,10 @@
 <template>
-  
+    <section>
+        <div class="container">
+            <span> --Content goes here-- </span>
+        </div>
+       
+    </section>
 </template>
 
 <script>
@@ -8,6 +13,15 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import "../assets/style/variables";
+    section {
+        display: flex;
+        justify-content: flex-start;
+        height: 130px;
+        background-color: #1c1c1c;
+        color: white;
+        font-weight: 700;
+         
+    }
 </style>
