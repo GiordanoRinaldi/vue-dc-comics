6945d22a6c624a2ebe228c75cd6f7679aa5cb566
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-30 16:42:01 +0200

    Aggiuinte "cards"

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ export default {
     Header,
     Top,
     Bottom,
-    Foooter
+    Foooter,
   }
 }
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="Card">
+        <div class="img-card">
+            <img :src="prod.thumb" :alt="prod.series">    
+        </div>
+        
+        <div class="title-card">
+            {{prod.series}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+    props: ["prod"]
+}
+</script>
+
+<style lang="scss" scoped>
+    .Card{
+        margin: auto;
+        width: calc(100% / 6);
+    }
+    .img-card{
+        width: 190px;
+        height: 190px;
+        overflow: hidden;
+            img{
+                width: 100%;
+                
+            }
+        
+        
+    }
+</style>
